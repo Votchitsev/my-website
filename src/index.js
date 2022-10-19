4d0830860781b5c9d4265377a322c31d60270df0
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-10-19 20:23:54 +0200

    contacts section

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './widgets/banner.widget/banner.widget.css';
 import './widgets/about-me.widget/about-me.widget.css';
 import './widgets/education.widget/education.widget.css';
 import './widgets/pet-projects.widget/pet-projects.widget.css';
+import './widgets/contact.widget/contact.widget.css';
 import './app';
